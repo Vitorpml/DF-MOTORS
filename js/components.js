64d98cbf3593f1583b2eed09210d18502f1ba611
Vitorpml <vitorg.pompollo@gmail.com>
2026-07-07 23:32:11 -0300

    update

--- js/components.js
+++ js/components.js
@@ -54,7 +54,7 @@ function renderFooter() {
           </div>
           <div class="footer-map">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3695.0!2d-48.799!3d-22.598!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjLCsDM1JzUyLjgiUyA0OMKwNDcnNTYuNCJX!5e0!3m2!1spt-BR!2sbr!4v1234567890"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2605.008710700499!2d-48.818738688331784!3d-22.57765777194938!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c0b1cf739718af%3A0xeb3f00a3bce3a98f!2sDF%20Motors!5e0!3m2!1spt-BR!2sus!4v1783477893697!5m2!1spt-BR!2sus"
               loading="lazy"
               title="Localização DF MOTORS"
               referrerpolicy="no-referrer-when-downgrade">
@@ -73,7 +73,7 @@ function renderFooter() {
 
           <p class="footer-col-title" style="margin-top:32px">Redes Sociais</p>
           <div class="social-links">
-            <a href="https://instagram.com/dfmotors" target="_blank" rel="noopener" class="social-link" title="Instagram">
+            <a href="https://www.instagram.com/dfmotors01/" target="_blank" rel="noopener" class="social-link" title="Instagram">
               <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
               </svg>
